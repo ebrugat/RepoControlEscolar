a29806a002895600463b566546ae7e4a835f6a72
Enric <xxbrugatxx@gmail.com>
2024-02-22 13:16:24 +0100

    Añado plantilla proyecto

---
+++ ControlEscolar/src/main/java/com/mycompany/controlescolar/main/ControlEscolar.java
@@ -0,0 +1,18 @@
+package com.mycompany.controlescolar.main;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author Mati
+ */
+public class ControlEscolar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
